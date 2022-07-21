dc7ac3e06d04b6a6c79c22c0ae74b4a85baf69e9
Michael Meoak <mmeoak240@gmail.com>
2022-07-20 19:43:10 -0700

    about to use db:reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,14 @@ puts "🌱 Seeding spices..."
     employment_type: Faker::Job.employment_type,
     education_level: Faker::Job.education_level
   )
-end
 
-application = Application.create(
-  name:
-  
+rand(1..3).times do
+ Application.create(
+  name: Faker::Name.unique.name,
+  email: Faker::Internet.unique.email,
+  job_id: job.id
 )
+end
+end
 
 puts "✅ Done seeding!"
