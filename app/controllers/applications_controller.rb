663f0874f80b6873a16daf0d5a637b46fc590332
Michael Meoak <mmeoak240@gmail.com>
2022-07-29 15:27:40 -0700

    added a job_id params to applications controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -10,6 +10,7 @@ class ApplicationsController < ApplicationController
     application = Application.create(
       name:params[:name],
       email:params[:email]
+      job_id: params[:job_id]
     )
   end
 
